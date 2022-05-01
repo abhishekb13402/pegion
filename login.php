74d49e9cba9c8d99345dff4f9cec06d7fc45b579
Rohit01-pro <rohit777kanojiya@gmail.com>
2022-05-01 09:54:03 +0530

    commit

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
         $email = mysqli_real_escape_string($connection, $_POST['email']);
         $pswd = md5(mysqli_real_escape_string($connection, $_POST['pswd']));
         
-        echo $email . "<br>". $pswd;
+        // echo $email . "<br>". $pswd;
 
         $query = "SELECT u_id, user_name, full_name FROM user WHERE email = '$email' AND password = '$pswd'";
         
